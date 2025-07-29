e4ced4e552eb09fdeeb3d66557f5998b47f1c534
NemchME <mnemch@yandex.ru>
2025-07-29 15:33:44 +0300

    Сontroller classes have been created

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,37 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/films")
 public class FilmController {
+    private final Map<Integer, Film> films = new HashMap<>();
+    private int nextId = 1;
+
+    @PostMapping
+    public Film addFilm(@RequestBody Film film) {
+        film.setId(nextId++);
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    @PutMapping
+    public Film updateFilm(@RequestBody Film film) {
+        if (!films.containsKey(film.getId())) {
+            throw new RuntimeException("Фильм с ID " + film.getId() + " не найден");
+        }
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    @GetMapping
+    public List<Film> getAllFilms() {
+        return new ArrayList<>(films.values());
+    }
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,37 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final Map<Integer, User> users = new HashMap<>();
+    private int nextId = 1;
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        user.setId(nextId++);
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @PutMapping
+    public User updateUser(@RequestBody User user) {
+        if (!users.containsKey(user.getId())) {
+            throw new RuntimeException("Пользователь с ID " + user.getId() + " не найден");
+        }
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return new ArrayList<>(users.values());
+    }
+}
